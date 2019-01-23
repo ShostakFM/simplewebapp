034683f2f8a0cf54951ac4aaa519dd638d72f599
Marina Shostak <fm.shostak@gmail.com>
2019-01-23 19:55:05 +0300

    some fixes in controller

--- src/main/java/com/godeltech/controller/EmployeeController.java
+++ src/main/java/com/godeltech/controller/EmployeeController.java
@@ -8,7 +8,9 @@ import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiResponse;
 import io.swagger.annotations.ApiResponses;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -28,15 +30,19 @@ public class EmployeeController {
     @ApiOperation(value = "Gets all employees", nickname = "EmployeeController.findAll")
     @ApiResponses(value = {@ApiResponse(code = 200, message = "Employees")})
     @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
-    public List<EmployeeDTO> findAll() {
-        return employeeService.findAll();
+    public ResponseEntity<List<EmployeeDTO>> findAll() {
+        List<EmployeeDTO> employees = employeeService.findAll();
+        return new ResponseEntity<>(employees, HttpStatus.OK);
     }
 
     @ApiOperation(value = "Gets employee by id", nickname = "EmployeeController.getById")
     @ApiResponses(value = {@ApiResponse(code = 200, message = "Employee")})
     @GetMapping(value = "/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
-    public EmployeeDTO getById(@PathVariable int id) {
-        return  employeeService.getById(id);
+    public ResponseEntity<EmployeeDTO> getById(@PathVariable int id) {
+        EmployeeDTO employeeDTO = employeeService.getById(id);
+        if (employeeDTO == null)
+            return new ResponseEntity<>(employeeDTO, HttpStatus.NOT_FOUND);
+        return new ResponseEntity<>(employeeDTO, HttpStatus.OK);
     }
 
     @ApiOperation(value = "Creates employee", nickname = "EmployeeController.create")
@@ -44,15 +50,25 @@ public class EmployeeController {
     @PostMapping(
             consumes = MediaType.APPLICATION_JSON_VALUE,
             produces = MediaType.APPLICATION_JSON_VALUE)
-    public void create (@RequestBody EmployeeDTO employeeDTO){
-        employeeService.create(employeeDTO);
+    public ResponseEntity<Void> create(@RequestBody EmployeeDTO employeeDTO) {
+        try {
+            employeeService.create(employeeDTO);
+            return new ResponseEntity<>(HttpStatus.CREATED);
+        } catch (Exception e) {
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }
     }
 
     @ApiOperation(value = "Deletes employee", nickname = "EmployeeController.delete")
     @ApiResponses(value = {@ApiResponse(code = 200, message = "Employee is deleted")})
     @DeleteMapping(value = "/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
-    public void delete (@PathVariable int id) {
-        employeeService.delete(id);
+    public ResponseEntity<Void> delete(@PathVariable int id) {
+        try {
+            employeeService.delete(id);
+            return new ResponseEntity<>(HttpStatus.OK);
+        } catch (Exception e) {
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }
     }
 
     @ApiOperation(value = "Edits employee", nickname = "EmployeeController.update")
@@ -60,7 +76,12 @@ public class EmployeeController {
     @PutMapping(
             consumes = MediaType.APPLICATION_JSON_VALUE,
             produces = MediaType.APPLICATION_JSON_VALUE)
-    public void update(@RequestBody EmployeeDTO employeeDTO){
-        employeeService.update(employeeDTO);
+    public ResponseEntity<Void> update(@RequestBody EmployeeDTO employeeDTO) {
+        try {
+            employeeService.update(employeeDTO);
+            return new ResponseEntity<>(HttpStatus.OK);
+        } catch (Exception e) {
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }
     }
 }
